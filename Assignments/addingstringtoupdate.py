541dd4f4376762428024abc57f50b14cd575bd12
Akash281296 <157715505+Akash281296@users.noreply.github.com>
2024-02-20 19:54:47 +0000

    class16 changes

---
+++ Assignments/addingstringtoupdate.py
@@ -0,0 +1,11 @@
+# 1. Try adding a string to set.update 
+
+
+
+my_set = {"apple", "banana", "cherry"}
+my_set.update("orange")
+print(my_set)  
+
+# Adding an existing element
+my_set.update("apple")
+print(my_set)  
---
+++ Assignments/enumeratefunction.py
@@ -0,0 +1,21 @@
+person_details = {
+    "name": "Narendra Modi",
+    "age": 67,
+    "salary": 2_00_000,
+    "role": "PM of India",
+}
+
+# Enumerate keys
+print("\nEnumerating keys:")
+for index, key in enumerate(person_details.keys()):
+    print(f"Key {index}: {key}")
+
+# Enumerate values
+print("\nEnumerating values:")
+for index, value in enumerate(person_details.values()):
+    print(f"Value {index}: {value}")
+
+# Enumerate items
+print("\nEnumerating items:")
+for index, (key, value) in enumerate(person_details.items()):
+    print(f"Item {index}: {key} - {value}")
